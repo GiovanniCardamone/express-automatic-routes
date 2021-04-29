f52ae64f2b64ae287b711a2648bd7df15cb3927e
David Silverthorn <david.martin.silverthorn@gmail.com>
2021-04-29 09:37:37 -0500

    Check if file type is acceptable
    I've added a simple check for whether the file ends in ".js" or ".ts" to avoid parsing errors when processing other file types. This allows us to store templates and other route related files alongside the route definition without the need to prepend a dot or underscore character to the filename.

--- src/index.ts
+++ src/index.ts
@@ -69,6 +69,7 @@ function scan(
 
 function isAcceptableFile(file: string, stat: fs.Stats): boolean {
   return (
+    ( file.endsWith('.js') || file.endsWith('.ts') ) &&
     !path.basename(file).startsWith('.') &&
     !path.basename(file).startsWith('_') &&
     !file.endsWith('.map') &&
